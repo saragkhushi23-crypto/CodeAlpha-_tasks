dd30eeebb222b881f9195f26b6ddf785ce4a12fc
saragkhushi23-crypto <saragkhushi23@gmail.com>
2026-02-27 12:52:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-let display = document.getElementById('display');
-function appendNumber(value) {
-    display.value+=value;
-}
-function clearDisplay() {
-     display.value ='';
-}
-function calculateResult(){
-    try {
-      display.value = eval(display.value);
-    }catch(e){
-        display.value = 'Error';
-        
-    }
-}
-document.addEventListener('keydown',function(event) {
-    const key = event.key;
-    if(!isNaN(key)||['+','-','*','/'].includes(key)){
-    appendNumber(key);
-    }else if (key==='Enter') {
-        calculateResult();
-    }else if(key ==='Escape'){
-        clearDisplay();
-    }
+const buttons = document.querySelectorAll(".filters button");
+const cards = document.querySelectorAll(".image-card");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const filter = button.getAttribute("data-filter");
+
+    // active button highlight
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    cards.forEach(card => {
+      const category = card.getAttribute("data-category");
+
+      if (filter === "all" || filter === category) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
 });
